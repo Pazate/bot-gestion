58c726a08572969edff29b28c1ba7e2cfacc115f
Pazate <40771235+Pazate@users.noreply.github.com>
2018-07-02 22:09:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 var prefix = ("*")
-
-bot.login("NDYzNDE1MTg5MTY2MDk2Mzg0.DhwRHA.7YEuUgFCfgUHaCBQhstuyqo1f2Y");
-
-
 bot.on('message',FUNCTION(message){
        if (message.content ==='!ping'){
     message.reply('pong')
 }
 })
+
+bot.login("NDYzNDE1MTg5MTY2MDk2Mzg0.DhwRHA.7YEuUgFCfgUHaCBQhstuyqo1f2Y");
+
+
 
 
