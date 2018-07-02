1b360bc2aa738860e0f95796b61aef69c46de257
Pazate <40771235+Pazate@users.noreply.github.com>
2018-07-03 00:09:33 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,6 +8,12 @@ bot.on('message', function (message){
 }
 })
 
+bot.on('guildMemberAdd', function(channel){
+       member.creatorDM().then(function(channel) {
+              return channe.send('Bienvenue sur le channel'+member.displayName)
+       }).catch(console.error)
+})
+
 bot.login("NDYzNDE1MTg5MTY2MDk2Mzg0.DhwRHA.7YEuUgFCfgUHaCBQhstuyqo1f2Y");
 
 
