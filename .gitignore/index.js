4d6a1d34e0be5f0454fa6e6463bbaac6a0934177
Pazate <40771235+Pazate@users.noreply.github.com>
2018-07-03 11:42:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,20 +1,27 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+
+//envie d un message qu en un utilisateur arrive
 bot.on('guildMemberAdd', function(member){
        member.createDM().then(function(channel) {
               return channel.send('Bienvenue sur le channel'+member.displayName)
        }).catch(console.error)
 })
-
+// enoi d un message pinf reponse pong
 bot.on('message', function (message){
        if (message.content ==='!ping'){
     message.reply('pong')
 }
 })
 
-
-
+//attribution des roles qu en quelqu un arrive sur le serveur 
+bot.on('guildMemberUpdate', function(member){
+    member.createDM().then(function(channel) {
+           return channel.send('Bienvenue sur le channel'+member.displayName)
+    }).catch(console.error)
+})
+//jeton 
 bot.login("NDYzNDE1MTg5MTY2MDk2Mzg0.DhwRHA.7YEuUgFCfgUHaCBQhstuyqo1f2Y");
 
 
