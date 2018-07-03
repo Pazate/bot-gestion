f7d31105fd01d403ed7350614358283407ef71ac
Pazate <40771235+Pazate@users.noreply.github.com>
2018-07-03 03:10:47 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+bot.on('guildMemberAdd', function(member){
+       member.creatorDM().then(function(channel) {
+              return channel.send('Bienvenue sur le channel'+member.displayName)
+       }).catch(console.error)
+})
 
 bot.on('message', function (message){
        if (message.content ==='!ping'){
@@ -8,11 +13,7 @@ bot.on('message', function (message){
 }
 })
 
-bot.on('guildMemberAdd', function(member){
-       member.creatorDM().then(function(channel) {
-              return channe.send('Bienvenue sur le channel'+member.displayName)
-       }).catch(console.error)
-})
+
 
 bot.login("NDYzNDE1MTg5MTY2MDk2Mzg0.DhwRHA.7YEuUgFCfgUHaCBQhstuyqo1f2Y");
 
