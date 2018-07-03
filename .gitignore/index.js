4aa155efc2f3d6c212f3c73dfc810b31847ffb45
Pazate <40771235+Pazate@users.noreply.github.com>
2018-07-03 02:28:56 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-var prefix = ("*")
+
 bot.on('message', function (message){
        if (message.content ==='!ping'){
     message.reply('pong')
